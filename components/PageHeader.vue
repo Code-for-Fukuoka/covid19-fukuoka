bf4f7f3935e52284addc0d571aa37fdf5e245a87
Yoshio Sakamoto <sakamoto@isit.or.jp>
2020-04-02 15:42:41 +0900

    Update PageHeader.vue

--- components/PageHeader.vue
+++ components/PageHeader.vue
@@ -9,6 +9,7 @@
     <div class="date">
       <span>最終更新 </span>
       <time :datetime="formattedDate">{{ date }}</time>
+      本サイトの情報は<a href="https://ckan.open-governmentdata.org/organization/fukuoka-pref" target="_blank" rel="noopener">福岡県オープンデータ</a>を1時間おきに確認して更新しています。
     </div>
   </div>
 </template>
