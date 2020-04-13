62316f478da14ac86b2229add4b9cb7b9bb7f6ce
Yoshio Sakamoto <sakamoto@isit.or.jp>
2020-04-13 09:01:01 +0900

    Update Summary

--- pages/index.vue
+++ pages/index.vue
@@ -18,6 +18,7 @@
           title="検査陽性者の状況"
           :title-id="'details-of-confirmed-cases'"
           :date="Data.inspections_summary.date"
+          :url="'https://ckan.open-governmentdata.org/dataset/401000_pref_fukuoka_covid19_totalpatients'"
         >
           <confirmed-cases-table v-bind="confirmedCases" />
         </svg-card>
