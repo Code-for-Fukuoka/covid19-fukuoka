ae2330a8d26a8f1b26a06a37953cf1ea6d737558
Yoshio Sakamoto <sakamoto@isit.or.jp>
2020-05-11 13:29:30 +0900

    add infomation

--- components/ConfirmedCasesTable.vue
+++ components/ConfirmedCasesTable.vue
@@ -87,7 +87,7 @@
     />
     <text
       class="black text"
-      transform="translate(10 95)"
+      transform="translate(15 95)"
       :dx="getAdjustX(検査実施人数)"
     >
       <tspan>{{ 検査実施人数 }}</tspan>
