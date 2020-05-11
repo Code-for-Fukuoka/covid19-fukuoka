21c5a94195bdc5001b6fe5bccd593baf98f28d11
Yoshio Sakamoto <sakamoto@isit.or.jp>
2020-05-11 18:16:00 +0900

    Update TimeStackedBarChart.vue

--- components/TimeStackedBarChart.vue
+++ components/TimeStackedBarChart.vue
@@ -16,7 +16,7 @@
         :unit="displayInfo.unit"
       />
     </template>
-    <small v-if="areaNum == 0" class="annotation">
+    <small class="annotation">
         ※ 福岡県は福岡市、北九州市以外の自治体の合計</br>
         ※ 自治体のラベルをクリックすることで特定の自治体のグラフを非表示にできます
     </small>
